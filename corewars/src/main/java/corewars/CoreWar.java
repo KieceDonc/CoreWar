82851ab87db7f57af559581474b728bd3a615ac0
RenardBOT <dinoking.36@gmail.com>
2021-02-22 16:33:36 +0100

    Ajout du projet maven

--- corewars/src/main/java/corewars/CoreWar.java
+++ corewars/src/main/java/corewars/CoreWar.java
@@ -1,15 +1,9 @@
 package corewars;
 
-/**
- * Hello world!
- */
-public class CoreWar {
-
-    /**
-     * Says hello to the world.
-     * @param args The arguments of the program.
-     */
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+public class CoreWar
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
     }
 }
