c739a0ef1b04c6af96280dce51d8c6feb2e1c11b
RenardBOT <dinoking.36@gmail.com>
2021-02-22 18:45:04 +0100

    Encapsulation de base des éléments du jeu

--- corewars/src/main/java/corewars/elements/Memoire.java
+++ corewars/src/main/java/corewars/elements/Memoire.java
@@ -4,7 +4,7 @@ package corewars.elements;
  * La mémoire est un tableau d'instructions dans lequel le code des deux joueurs évolue.
  */
 
-public class Mémoire {
+public class Memoire {
 
     public Instruction[] tabIns;
     
