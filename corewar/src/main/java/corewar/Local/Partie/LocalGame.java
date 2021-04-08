360c114afd1d779ef301a4449b9900cb55ae7ae2
KieceDonc <valentin.verstracte@gmail.com>
2021-04-08 14:49:19 +0200

    push

--- corewar/src/main/java/corewar/Local/Partie/LocalGame.java
+++ corewar/src/main/java/corewar/Local/Partie/LocalGame.java
@@ -1,6 +1,7 @@
 package corewar.Local.Partie;
 import corewar.Local.elementsCore.*;
 
+import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
 import corewar.Local.Interpreteur.*;
@@ -9,45 +10,37 @@ public class LocalGame {
 
     Core core;
 
-    public LocalGame(){
+    public LocalGame() {
         this.core = null;
     }
 
-
-
-    public void localTest(){
+    public void localTest() {
         /*
-        Core c = new Core(420);
-        InstructionID i1 = new InstructionID(Mnemonique.ADD, Mode.IMMEDIAT, 0, Mode.DIRECT, 3, 'A');
-        InstructionID i2 = new InstructionID(Mnemonique.MOV, Mode.DIRECT, 2, Mode.INDIRECT, 2, 'A');
-        InstructionID i3 = new InstructionID(Mnemonique.JMP, Mode.DIRECT, -2, 'A');
-        InstructionID i4 = new InstructionID(Mnemonique.DAT, Mode.IMMEDIAT, 0, 'A');
-        c.write(600,i1);
-        c.write(601,i2);
-        c.write(602,i3);
-        c.write(603,i4);
-        System.out.println(c.toString()+"\n");
-        System.out.println(i1.toString());
-        System.out.println(i2.toString());
-        System.out.println(i3.toString());
-        System.out.println(i4.toString());
-        */
+         * Core c = new Core(420); InstructionID i1 = new InstructionID(Mnemonique.ADD,
+         * Mode.IMMEDIAT, 0, Mode.DIRECT, 3, 'A'); InstructionID i2 = new
+         * InstructionID(Mnemonique.MOV, Mode.DIRECT, 2, Mode.INDIRECT, 2, 'A');
+         * InstructionID i3 = new InstructionID(Mnemonique.JMP, Mode.DIRECT, -2, 'A');
+         * InstructionID i4 = new InstructionID(Mnemonique.DAT, Mode.IMMEDIAT, 0, 'A');
+         * c.write(600,i1); c.write(601,i2); c.write(602,i3); c.write(603,i4);
+         * System.out.println(c.toString()+"\n"); System.out.println(i1.toString());
+         * System.out.println(i2.toString()); System.out.println(i3.toString());
+         * System.out.println(i4.toString());
+         */
 
         Warrior J1 = new Warrior();
         pr("j1 test");
         J1.setNom("J1");
         J1.setId('1');
-        J1.setInstructions(Interpreteur.interpreter("corewar/src/main/java/corewar/Warriors/imp.redcode"),J1.getId());
+        J1.setInstructions(Interpreteur.interpreter("corewar/src/main/java/corewar/Warriors/imp.redcode"), J1.getId());
         J1.setCouleur("blue");
         pr("j1");
 
         Warrior J2 = new Warrior();
         J2.setNom("J2");
         J2.setId('2');
-        J2.setInstructions(Interpreteur.interpreter("corewar/src/main/java/corewar/Warriors/imp.redcode"),J2.getId());
+        J2.setInstructions(Interpreteur.interpreter("corewar/src/main/java/corewar/Warriors/imp.redcode"), J2.getId());
         J2.setCouleur("red");
         pr("j2");
-
 
         Warriors w = new Warriors();
         w.add(J1);
@@ -57,17 +50,16 @@ public class LocalGame {
         c.setWarriors(w);
         c.load();
 
-        Manche m = new Manche(w,c);
+        Manche m = new Manche(w, c);
 
         m.traitementPartie(100);
     }
 
-    
-    public static void pr(Object o){
+    public static void pr(Object o) {
         System.out.println(o);
     }
 
-    public static void wait(int ms){
+    public static void wait(int ms) {
         try {
             TimeUnit.MILLISECONDS.sleep(ms);
         } catch (InterruptedException e) {
@@ -76,11 +68,12 @@ public class LocalGame {
         }
     }
 
-    public static void clearScreen() {  
+    public static void clearScreen() {
+        wait(2000);
 
-        System.out.print("\033[H\033[2J");  
-     
-        System.out.flush();  
+        for(int x=0;x<50;x++){
+            System.out.println("\n");
+        }
      
      }
     
